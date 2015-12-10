6e40ea12a4b32db31266e3eb010b6bbd4c268c64
Renwallz <renwick90@gmail.com>
2015-12-10 17:10:22 +1100

    Day 9 part 1 brute force method

---
+++ python/day9.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+from collections import defaultdict
+
+places = defaultdict(dict)
+
+with open("day9.in") as f:
+    for line in f:
+        line = line.strip()
+        first, second, distance = line.split()[::2]
+        places[first][second] = int(distance)
+        places[second][first] = int(distance)
+
+
+#brute force method  
+mindistance = 9999999   
+combo = tuple()   
+for permi in permutations(places.keys(), len(places.keys())):
+    distance = 0
+    for i in range(len(permi)-1):
+        distance += places[permi[i]][permi[i+1]]
+    if distance < mindistance:
+        combo = permi
+        mindistance = distance
+        
+print (combo, mindistance)
