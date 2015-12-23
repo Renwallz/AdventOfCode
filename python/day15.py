a1510e28ae435dbd4de14ffd1a36ea014dc4d5b2
renwallz <renwick90@gmail.com>
2015-12-23 16:01:13 +1100

    Day 15 part 1, brute force approach

---
+++ python/day15.py
@@ -0,0 +1,49 @@
+from functools import reduce
+from itertools import product
+from operator import mul
+
+inp = '''Sprinkles: capacity 5, durability -1, flavor 0, texture 0, calories 5
+PeanutButter: capacity -1, durability 3, flavor 0, texture 0, calories 1
+Frosting: capacity 0, durability -1, flavor 4, texture 0, calories 6
+Sugar: capacity -1, durability 0, flavor 0, texture 2, calories 8'''.split('\n')
+
+ATTRIBUTES = ['capacity', 'durability', 'flavor', 'texture']
+ingredients = {}
+
+TOTAL_TEASPOONS = 100
+
+
+def score(recipe):
+    if sum(recipe.values()) != TOTAL_TEASPOONS:
+        return 0
+
+    scores = {}
+    for item in recipe.keys():
+        for attr in ATTRIBUTES:
+            scores[attr] = scores.get(attr, 0) + recipe[item] * ingredients.get(item)[attr]
+
+    for item in scores.keys():
+        scores[item] = max(0, scores[item])
+
+    return reduce(mul, scores.values())
+
+
+for line in inp:
+    line = line.replace(',', '')
+    line = line.replace(':', '')
+    line = line.split()
+
+    ingredients[line[0]] = dict(zip(line[1::2], map(int, line[2::2])))
+
+scores = []
+
+for amounts in product(range(TOTAL_TEASPOONS), repeat=len(ingredients.keys())):
+    if sum(amounts) != TOTAL_TEASPOONS:
+        continue
+    recipe = dict(zip(ingredients.keys(), amounts))
+    scores.append((score(recipe), recipe))
+
+print(sorted(scores, key=lambda x: x[0], reverse=True)[:5])
+
+
+# print(score({'Sprinkles':25, 'PeanutButter': 25, 'Frosting': 25, 'Sugar': 25}))
